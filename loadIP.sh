47e81799ccdfc50c92ed94fb5bbdc1acc7df839a
Marek <mareksr@hm.pl>
2016-03-07 20:39:10 +0100

    ok

--- loadIP.sh
+++ loadIP.sh
@@ -2,10 +2,9 @@
 
 ipset flush blacklist
 
-wget https://raw.githubusercontent.com/mareksr/spamip/master/black.ip.10 -O /tmp/black.ip.10 
-cat /tmp/black.ip.10  | while read ip
+curl https://raw.githubusercontent.com/mareksr/spamip/master/black.ip.10  | while read ip
 do
-	ip2=`echo $ip |sed "s/ .*//g"`
-	ipset add blacklist $ip2
+        ip2=`echo $ip |sed "s/ .*//g"`
+        ipset add blacklist $ip2
 done
 
